e883546654977c272c6acd0abcf21ffe8d76701e
Shivam Shah <shivshah12698@gmail.com>
2023-12-02 11:18:28 -0500

    Added new model still same issue.

--- Models/ConvNextT.py
+++ Models/ConvNextT.py
@@ -1,55 +1,12 @@
-#import torch
-#from datasets import load_dataset
-#from transformers import EfficientNetImageProcessor, EfficientNetForImageClassification
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import EfficientNetB4
+from tensorflow.keras.applications.convnext import ConvNeXtTiny
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input
-#from Utility.CroppingImage import resize_and_save
-
+import os
 import numpy as np
 
-from PIL import Image
-import os
 
-# def resize_and_save(input_folder, output_folder, new_width, new_height):
-#     # Create output folder if it doesn't exist
-#     if not os.path.exists(output_folder):
-#         os.makedirs(output_folder)
-
-#     # Loop through all files in the input folder
-#     for filename in os.listdir(input_folder):
-#         if filename.endswith(('.png', '.jpg', '.jpeg', '.gif')):  # Add more extensions if needed
-#             # Construct full file paths
-#             input_path = os.path.join(input_folder, filename)
-
-#             # Open the image
-#             image = Image.open(input_path)
-
-#             # Resize the image
-#             resized_image = image.resize((new_width, new_height), Image.LANCZOS)
-
-#             # Get the file name (without extension) from the input path
-#             file_name = os.path.splitext(filename)[0]
-
-#             # Save the resized image to the output folder
-#             output_path = os.path.join(output_folder, f"{file_name}_resized.jpg")
-#             resized_image.save(output_path)
-
-
-# # Example usage
-
-# input_folder_path_train = "images/preprocessed/train"
-# output_folder_path_train = "images/preprocessed/train_cropped"
-# input_folder_path_test = "images/preprocessed/test"
-# output_folder_path_test = "images/preprocessed/test_cropped"
-# new_width = 255  # Set the desired width
-# new_height = 255  # Set the desired height
-
-# resize_and_save(input_folder_path_train, output_folder_path_train, new_width, new_height)
-# resize_and_save(input_folder_path_test, output_folder_path_test, new_width, new_height)
-
-def EfficentNetB4():
+def ConvNextT():
     datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
@@ -59,20 +16,20 @@ def EfficentNetB4():
     
     train_generator = datagen.flow_from_directory(
     directory = 'images/preprocessed/train_cropped/',
-    target_size=(380, 380),
+    target_size=(225, 225),
     batch_size=32,
     class_mode = None
 
 )
     
-    model = EfficientNetB4(weights='imagenet', include_top=False, input_shape=(255, 255, 3))
+    model = ConvNeXtTiny(weights='imagenet', include_top=False, input_shape=(255, 255, 3))
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     model.fit(train_generator, epochs=10)
     
     # Assuming you have a test generator as well
     test_generator = datagen.flow_from_directory(
     'images/preprocessed/test_cropped/',
-    target_size=(224, 224),
+    target_size=(225, 225),
     batch_size=32,
     class_mode = None
     )
@@ -121,7 +78,7 @@ def preProcessData(input_directory):
             preprocessed_images.append(img_array)
     return np.vstack(preprocessed_images)
 
-def EfficentNetB4V2():
+def ConvNextTV2():
     input_folder_path_train = "images/preprocessed/train_cropped"
     # output_folder_path_train = "images/preprocessed/train_cropped"
     input_folder_path_test = "images/preprocessed/test_cropped"
@@ -142,7 +99,7 @@ def EfficentNetB4V2():
     
 
    
-    model = EfficientNetB4(weights='imagenet', include_top=False, input_shape=(255, 255, 3))
+    model = ConvNeXtTiny(weights='imagenet', include_top=False, input_shape=(255, 255, 3))
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     model.fit(X, epochs=10)
     
@@ -179,4 +136,4 @@ def EfficentNetB4V2():
 
 
 
-EfficentNetB4()
+ConvNextT()
